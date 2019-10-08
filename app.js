8c0d4590dee13201b5bbb4f0b38d115e0b3f38e5
Ephriam Henderson <orik162@gmail.com>
2019-10-08 10:10:39 -0500

    Lesson

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
+app.get("/movies", (req, res) => {
+    res.send("Movies")
+})
+
+app.get("/action", (req, res) => {
+    res.send("Action")
+})
+
 app.listen(PORT, () => {
     console.log(`Express has started on Port ${PORT}`)
 })
